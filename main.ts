d369860bc7a833a2ee38bf3f2a6c76954c219bfc
Thomas Sparks <69657545+thsparks@users.noreply.github.com>
2025-03-19 17:15:15 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,23 @@
+function doSomething (num: number, bool: boolean, text: string) {
+	
+}
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+let list = [mySprite, 1, "Hello".length]
+controller.player1.moveSprite(null)
